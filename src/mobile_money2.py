e24bcdc957f1c820fe62584fa8bb009e25867869
sherry flavia <nanderesherinaflavia@gmail.com>
2026-09-15 17:20:56 +0300

    updated class transaction

--- src/mobile_money2.py
+++ src/mobile_money2.py
@@ -3,9 +3,18 @@ class Customer:
 
 class Account:
     pass
+class Transaction:
+    def __init__(self, transaction_id, transaction_type, amount, date_time):
+        self.transaction_id = transaction_id
+        self.transaction_type = transaction_type
+        self.amount = amount
+        self.date_time = date_time
 
-class Transactions:
-    pass
+    def get_details(self):
+        return f"Transaction ID: {self.transaction_id}, " \
+               f"Type: {self.transaction_type}, " \
+               f"Amount: UGX {self.amount}, " \
+               f"Date: {self.date_time}"
 
 class TransactionHistory:
     pass
