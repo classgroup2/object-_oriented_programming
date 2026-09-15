db908e27a0983f6f2333714059466371a08b856f
Obar Daniel <obardaniel01@gmail.com>
2026-09-15 17:51:34 +0300

    add the the agent class

--- src/mobile_money2.py
+++ src/mobile_money2.py
@@ -29,7 +29,7 @@ class TransactionHistory:
             return
         for i, t in enumerate(self.transactions, 1):
             print(f"{i}. {t.get_details()}")
-
+#obar
 class Agent:
     def __init__(self, name, agent_id, branch):
         self._name = name
